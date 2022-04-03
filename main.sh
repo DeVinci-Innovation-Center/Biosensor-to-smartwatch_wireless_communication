41e219fd9b06ddbfccef558a9acea00e311e4670
Vivien Perrelle <vivien.perrelle@edu.devinci.fr>
2022-04-03 18:07:09 +0200

    Delete main.sh

--- main.sh
+++
@@ -1,6 +0,0 @@
-/usr/bin/python3 biosensor/get_ecflex_data.py  # Execute in parallel
-# /usr/bin/python3 biosensor/database_manager.py 
-
-# arduino --upload smartwatch/wifi_client/wifi_client.ino --port /dev/ttyUSB*
-
-# tail -f /dev/null
